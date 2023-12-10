15830157df80dd0c8c8bb4e803a4a6a5a6071ac3
senascrum3 <153387759+senascrum3@users.noreply.github.com>
2023-12-09 22:54:36 -0500

    Add files via upload

---
+++ Bicilab/Header.php
@@ -0,0 +1,17 @@
+<head>
+<link href="resources/css/header.css" rel="stylesheet" type="text/css"/>
+</head>
+<header>
+    <img class="imagen1" src="img/logo3.jpg" alt="logo">
+        <img class="imagen2" src="img/logo3.jpg" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="DahsBoard.php">Menú Principal</a></li>
+                <li><a href="sessionout.php">Salir</a></li>
+
+            </ul>
+        </nav> 
+</header>
+<?php 
+include './session.php';
+?>
---
+++ Bicilab/footer.php
@@ -0,0 +1,6 @@
+<head>
+<link href="resources/css/footer.css" rel="stylesheet" type="text/css"/>
+</head>
+<footer>
+        <p><center>Copyright © 2023</center></p>
+ </footer>
